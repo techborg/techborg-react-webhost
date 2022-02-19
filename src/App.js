1873c65375872d23192d2fa847b07c3d75cd66e8
shaunsebastian <shaunsebastian4@gmail.com>
2022-02-19 05:18:24 -0800

    git pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import {BrowserRouter as Router,Route} from 'react-router-dom'
 /**
  * ?  =====Import Components=====
  */
-import Signup from './Pages/Signup';
-import Home from './Pages/Home';
+import Signup from './Pages/Signup'
+import Home from './Pages/Home'
 import Login from './Pages/Login'
+import Robotics from './Pages/robotics'
+import Drones from './Pages/drones'
+import AI from './Pages/AI';
+import Blockchain from './Pages/blockchain'
 function App() { 
   return (
     <div>
@@ -23,7 +27,18 @@ function App() {
          <Route  path='/login'>
           <Login/>
          </Route>
-         
+         <Route  path='/robotics'>
+         <Robotics />
+         </Route>
+         <Route  path='/drones'>
+         <Drones />
+         </Route>
+         <Route  path='/AI'>
+         <AI />
+         </Route>
+         <Route  path='/blockchain'>
+         <Blockchain />
+         </Route>         
            
       </Router>
       
