f2ba8cba430118e0e09197a8591bd598fd141640
mahesh <maheshmb7712@gmail.com>
2022-04-07 21:52:39 +0530

    login started

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Login.css';
 import {useHistory} from 'react-router-dom'
-
+import { useContext } from 'react';
+import {FirebaseContext} from '../../store/FirebaseContext'
 function Login() {
-  const history = useHistory()
+const [email,setemail]=useState('')
+const [password,setpassword]=useState('')
+const {firebase}=useContext(FirebaseContext)
+const history =useHistory()
+const handlelognin=(e)=>{
+     e.preventDefault()
+     firebase.auth().signInWithEmailAndPassword(email,password).then(()=>{
+       history.push('/')
+     }).catch((error)=>{
+       alert(error.messege)
+     })
+}
   return (
     <div>
       <div className="loginParentDiv">
         
-        <form>
-          <label htmlFor="fname">Username/Email</label>
+        <form onSubmit={handlelognin}>
+          <label htmlFor="fname">Email</label>
           <br />
           <input
             className="input"
@@ -17,6 +29,8 @@ function Login() {
             id="fname"
             name="email"
             defaultValue="John@example.com"
+            value={email}
+            onChange={(e)=>setemail(e.target.value)}
           />
           <br />
           <label htmlFor="lname">Password</label>
@@ -27,12 +41,14 @@ function Login() {
             id="lname"
             name="password"
             defaultValue="Abc@123"
+            value={password}
+            onChange={(e)=>setpassword(e.target.value)}
           />
           <br />
           <br />
           <button>Login</button>
         </form>
-        <a onClick={()=>history.push('/signup')}>Signup</a>
+        <a>Signup</a>
       
       </div>
    
