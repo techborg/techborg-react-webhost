8d6117e710389a2832886578364b9e9d808b95de
shaunsebastian <shaunsebastian4@gmail.com>
2022-04-08 07:47:03 -0700

    Social Icons added in footers

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -52,8 +52,17 @@ function Footer() {
       
       <div className="footer">
         
-        <p onClick={()=>history.push('/')}>TechBorg © 2018-2022</p>
-        <SocialIcon  url="https://twitter.com/jaketrent"/>
+        <p onClick={()=>history.push('/')}>TechBorg Digital Labs © 2018-2022</p>
+        <div>
+        <SocialIcon  url="https://www.facebook.com/techborgfb/"/>
+        <SocialIcon url="https://www.instagram.com/techborg_/"/>
+        <SocialIcon url="https://twitter.com/tech_borg/"/>
+        <SocialIcon url="https://www.linkedin.com/company/techborglearning/"/>
+        <SocialIcon url="https://www.youtube.com/c/TechBorg"/>
+        <SocialIcon url="https://play.google.com/store/apps/details?id=com.techborg.techborgtutorial"/>
+        <SocialIcon url="https://github.com/techborg"/>
+        </div>
+        
   
         </div>
       </div>
--- src/Components/Footer2/Footer2.js
+++ src/Components/Footer2/Footer2.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Footer2.css';
 import {useHistory} from 'react-router-dom'
-
+import { SocialIcon } from 'react-social-icons';
 
 
 function Footer2() {
@@ -16,8 +16,16 @@ function Footer2() {
       
       <div className="footer">
         
-        <p onClick={()=>history.push('/')}>TechBorg © 2018-2022</p>
-        
+        <p onClick={()=>history.push('/')}>TechBorg Digital Labs © 2018-2022</p>
+        <div>
+        <SocialIcon  url="https://www.facebook.com/techborgfb/"/>
+        <SocialIcon url="https://www.instagram.com/techborg_/"/>
+        <SocialIcon url="https://twitter.com/tech_borg/"/>
+        <SocialIcon url="https://www.linkedin.com/company/techborglearning/"/>
+        <SocialIcon url="https://www.youtube.com/c/TechBorg"/>
+        <SocialIcon url="https://play.google.com/store/apps/details?id=com.techborg.techborgtutorial"/>
+        <SocialIcon url="https://github.com/techborg"/>
+        </div>
   
         </div>
       </div>
