96b7c0aa9fa7488ee9f142207528e627afd74dba
shaunsebastian <shaunsebastian4@gmail.com>
2022-04-09 23:17:55 -0700

    Error solved

--- src/store/FirebaseContext.js
+++ src/store/FirebaseContext.js
@@ -1,15 +1,18 @@
+import React from 'react'
 import { createContext,useState } from "react";
- export const FirebaseContext =createContext(null)
 
- export const Authcontext=createContext()
- export default function Context({children})
+export const FirebaseContext =createContext(null)
+
+export const Authcontext=createContext()
+ 
+export default function Context({children})
   {
      const [user,setuser]=useState()
    return (
-     (<div>
+     (
      <Authcontext.Provider value={{user,setuser}}>
       {children}
      </Authcontext.Provider>
-     </div>)
+     )
    )
  }
